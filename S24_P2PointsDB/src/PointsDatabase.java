69f2160f690c355424eb9071976b0d53e594b926
RichardMartinez <rmartinez21@vt.edu>
2024-03-06 18:56:52 -0500

    add honor code statement

--- S24_P2PointsDB/src/PointsDatabase.java
+++ S24_P2PointsDB/src/PointsDatabase.java
@@ -2,6 +2,22 @@
 // This is the basic starting code for CS 3114 Project 2
 // Richard Martinez
 
+// On my honor:
+// - I have not used source code obtained from another student,
+// or any other unauthorized source, either modified or unmodified.
+// - All source code and documentation used in my program is
+// either my original work, or was derived by me from the
+// source code published in the textbook for this course.
+// - I have not discussed coding details about this project with
+// anyone other than the my partner, instructor, ACM/UPE tutors 
+// or the TAs assigned to this course. 
+// I understand that I may discuss the concepts
+// of this program with other students, and that another student
+// may help me debug my program so long as neither of us writes
+// anything during the discussion or modifies any computer file
+// during the discussion. I have violated neither the spirit nor
+// letter of this restriction.
+
 public class PointsDatabase {
 
     public static void main(String[] args) {
