407d17ddb16024153ed8d5df7538792517448bb0
RichardMartinez <rmartinez21@vt.edu>
2024-03-12 19:59:30 -0400

    add java docs for main method

--- S24_P2PointsDB/src/PointsDatabase.java
+++ S24_P2PointsDB/src/PointsDatabase.java
@@ -1,6 +1,6 @@
-
-// This is the basic starting code for CS 3114 Project 2
-// Richard Martinez
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 // On my honor:
 // - I have not used source code obtained from another student,
@@ -18,8 +18,34 @@
 // during the discussion. I have violated neither the spirit nor
 // letter of this restriction.
 
+/**
+ * The class containing the main method, the entry point of the application. It
+ * will take a command line file argument which include the commands to be read
+ * and creates the appropriate SkipList object and outputs the correct results
+ * to the console as specified in the file.
+ *
+ * @author CS Staff, Richard Martinez
+ * 
+ * @version 2024-01-22
+ */
 public class PointsDatabase {
-
+    /*
+     * Compiled using JRE 11 for CS 3114
+     * Operating System: Windows 11
+     * IDE: Eclipse
+     * Date Completed: TODO
+     * Created By: Richard Martinez
+     * 
+     * TODO: Description
+     */
+    
+    /**
+     * The entry point of the application.
+     *
+     * @param args
+     *            The name of the command file passed in as a command line
+     *            argument.
+     */
     public static void main(String[] args) {
         System.out.println("This is working PDB");
     }
