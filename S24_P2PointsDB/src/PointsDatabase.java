1edf9ca127157625347465595e20f8f0a3455dc5
RichardMartinez <rmartinez21@vt.edu>
2024-03-12 20:07:29 -0400

    read from file and print

--- S24_P2PointsDB/src/PointsDatabase.java
+++ S24_P2PointsDB/src/PointsDatabase.java
@@ -47,7 +47,48 @@ public class PointsDatabase {
      *            argument.
      */
     public static void main(String[] args) {
-        System.out.println("This is working PDB");
+        // System.out.println("This is working PDB");
+        
+        if (args.length < 1) {
+            String out = "Invalid file. No filename in command line arguments";
+            System.out.println(out);
+            return;
+        }
+        
+        // the file containing the commands
+        File file = null;
+
+        // Attempts to open the file and scan through it
+        try {
+
+            // takes the first command line argument and opens that file
+            file = new File(args[0]);
+
+            // creates a scanner object
+            Scanner scanner = new Scanner(file);
+
+            // creates a command processor object
+            //CommandProcessor cmdProc = new CommandProcessor();
+
+            // reads the entire file and processes the commands
+            // line by line
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                // determines if the file has more lines to read
+                if (!line.trim().isEmpty()) {
+                    //cmdProc.processor(line.trim());
+                    System.out.println(line);
+                }
+            }
+            // closes the scanner
+            scanner.close();
+        }
+        // catches the exception if the file cannot be found
+        // and outputs the correct information to the console
+        catch (FileNotFoundException e) {
+            System.out.println("Invalid file");
+            e.printStackTrace();
+        }
     }
 
 }
