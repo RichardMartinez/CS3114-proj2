4e21a5da261e01ecb66af56fabae489ac5374832
RichardMartinez <rmartinez21@vt.edu>
2024-03-16 01:21:14 -0400

    16 points seems working

--- S24_P2PointsDB/src/PRQuadTreeTest.java
+++ S24_P2PointsDB/src/PRQuadTreeTest.java
@@ -94,8 +94,99 @@ public class PRQuadTreeTest extends TestCase {
         pt = new Point(384, 384);
         pair = new KVPair<String, Point>(name, pt);
         tree.insert(pair);
+
+        // TODO: test with dump()?
+    }
+    
+    /**
+     * Test sixteen points with multiple subdivisions
+     */
+    public void testSixteenPoints() {
+        String name;
+        Point pt;
+        KVPair<String, Point> pair;
         
+        name = "A";
+        pt = new Point(64, 64);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
         
+        name = "B";
+        pt = new Point(192, 64);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "C";
+        pt = new Point(64, 192);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "D";
+        pt = new Point(192, 192);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "E";
+        pt = new Point(64, 832);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "F";
+        pt = new Point(192, 832);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "G";
+        pt = new Point(64, 960);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "H";
+        pt = new Point(192, 960);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "I";
+        pt = new Point(832, 64);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "J";
+        pt = new Point(960, 64);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "K";
+        pt = new Point(832, 192);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "L";
+        pt = new Point(960, 192);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "M";
+        pt = new Point(832, 832);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "N";
+        pt = new Point(960, 832);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "O";
+        pt = new Point(832, 960);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        name = "P";
+        pt = new Point(960, 960);
+        pair = new KVPair<String, Point>(name, pt);
+        tree.insert(pair);
+        
+        // TODO: test with dump
     }
 
 }
